ba35d01be7f21725a611cc869f535b4a8c8f4470
Ian Chen <ian-@gmx.com>
2019-01-30 19:08:43 +0800

    FALALALA

--- src/components/shooting-stars/shooting-stars.tsx
+++ src/components/shooting-stars/shooting-stars.tsx
@@ -1,6 +1,4 @@
 import { Component, Prop, Element } from '@stencil/core';
-import { start } from 'repl';
-import { strict } from 'assert';
 
 @Component({
   tag: 'shooting-stars',
@@ -11,43 +9,53 @@ export class MyComponent {
 
   // Host element
   @Element() el: HTMLElement;
-
   @Prop() image: string;
-  @Prop() height: string = "10px";
-  @Prop() width: string = "10px";
+  @Prop() height: string = "30px";
+  @Prop() width: string = "30px";
   @Prop() concentration: number = 100;
 
   componentDidLoad() {
-
+    this.onInit();
+    console.log('here')
   }
 
-  private shootingStars = [];
+  // private shootingStars = [];
 
   onInit() {
-    const extension = this.el.clientHeight / this.el.clientWidth;
+    this.el.style.height = '100%';
+    this.el.style.width = '100%';
+    this.el.style.display = 'block';
     const totalWidth = this.el.clientHeight + this.el.clientWidth;
+    const background = this.el.shadowRoot.querySelector('#background');
     const numStars = Math.ceil(totalWidth / this.concentration);
     for (let i = 0; i < numStars; i++) {
+      console.log("generated")
       let star = document.createElement("img");
       star.className = "star";
       star.src = this.image;
       star.style.height = this.height;
       star.style.width = this.width;
       star.style.top = "-" + this.height;
-      star.style.left = this.calculateX(i, totalWidth)
-      document.body.appendChild(star);
+      star.style.left = this.calculateX(i);
+      star.style.animationDelay = this.randomizeSeconds(0, 10, true);
+      star.style.animationDuration = this.randomizeSeconds(2, 8, false)
+      background.appendChild(star);
     }
   }
 
-  private calculateX(index, totalWidth) {
-    return ((index + Math.random) * this.concentration) / totalWidth * 100 + "%";
+  private calculateX(index) {
+    return (index + Math.random()) * this.concentration + 'px';
   }
+
+  private randomizeSeconds(start, end, negative) {
+    let seconds = (Math.random() * end) + start + 's';
+    return negative ? '-' + seconds : seconds;
+  }
+
 
   render() {
     return (
       <div id="background">
-        {this.image}
-        <img src={this.image} class="shooting-star"></img>
       </div>
     );
   }
